0035253bd034adc6c0c540c1361f6909a7859f49
naxifa <maliha1@usf.edu>
2024-10-20 14:48:39 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+// Task 2 - Add Event Listeners for Product Selection 
+
+let selectSize = document.getElementById("select-size");
+let productPrice = document.getElementById("product-price");
+let purchaseButton = document.getElementById("purchase-button");
+
+selectSize.addEventListener("click", (event) => {
+    let selectedOption = event.target.selectedOptions[0];
+    let price = selectedOption.getAttribute("data-price");
+    
+        // Update the product price displayed
+        productPrice.textContent = `$${price}.00`;
+    });
